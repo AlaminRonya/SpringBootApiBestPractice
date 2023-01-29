e73b2d750fd6f2766f61a1afa26ef2b6ac52c600
AlaminRonya <hprony12@gmail.com>
2023-01-29 20:45:14 +0600

    initial exception handling:

---
+++ exceptionHandlingGlobal/src/main/java/com/alamin/exceptionhandlingglobal/ExceptionHandlingGlobalApplication.java
@@ -0,0 +1,13 @@
+package com.alamin.exceptionhandlingglobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingGlobalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionHandlingGlobalApplication.class, args);
+    }
+
+}
---
+++ exceptionHandlingGlobal/src/test/java/com/alamin/exceptionhandlingglobal/ExceptionHandlingGlobalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamin.exceptionhandlingglobal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlingGlobalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
